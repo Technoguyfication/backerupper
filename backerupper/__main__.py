c4ee42b0f286c28d498e26fd86bf97c3c32ccfb4
Hayden Andreyka <haydenandreyka@gmail.com>
2025-08-03 00:52:06 -0700

    nonfuntional multipart uploads

--- backerupper/__main__.py
+++ backerupper/__main__.py
@@ -1,3 +1,5 @@
+import random
+
 from backerupper.s3 import S3
 from backerupper.conf import Config
 
@@ -9,10 +11,32 @@ def main():
         aws_region=Config.aws_s3_region_name()
     )
 
-    buckets = s3_client.list_buckets()
-    print("Buckets:")
-    for bucket in buckets:
-        print(f"- {bucket.name} (Created on: {bucket.creation_date})")
+    # buckets = s3_client.list_buckets()
+    # print("Buckets:")
+    # for bucket in buckets:
+    #     print(f"- {bucket.name} (Created on: {bucket.creation_date})")
+
+    # objects = s3_client.list_objects(bucket_name=Config.aws_s3_bucket_name())
+    # for obj in objects:
+    #     print(f"Object: {obj}")
+
+    # load object from disk an upload to s3
+
+    # with open(r"C:\Users\Hayden\Downloads\Baked-Beans-22.png", 'rb') as file:
+    #     all_bytes = file.read()
+    #     s3_client.put_object(Config.aws_s3_bucket_name(), 'beans.png', all_bytes)
+
+    # s3_client.delete_object(Config.aws_s3_bucket_name(), 'beans.png')
+
+    upload = s3_client.create_multipart_upload(Config.aws_s3_bucket_name(), f'test-upload-{random.randint(100, 1000)}')
+    for i in range(10):
+        # create 10MB chunk of data
+        data = bytes([i] * int(10e6))
+        part_num, etag = upload.upload_part(data)
+        print(f"part {part_num} uploaded, etag: {etag}")
+    
+    completed = upload.complete()
+    print(f"Completed multipart upload, etag: {completed.etag}")
 
 if __name__ == "__main__":
     main()
--- backerupper/s3.py
+++ backerupper/s3.py
@@ -18,23 +18,23 @@ class S3:
 
         # set up session
         self._session = requests.Session()
-        self._session.headers.update({
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        })
     
-    def _request(self, method: str, path: str, params=None, data=None):
+    def _request(self, method: str, path: str, params: dict | None = None, data: bytes | None = None):
         url = urljoin(self._endpoint_url, path)
 
-        response = self._session.request(method, url, params=params, json=data, auth=self.auth)
+        response = self._session.request(method, url, params=params, data=data, auth=self.auth)
         response.raise_for_status()
-        if response.headers.get('Content-Type') == 'application/xml':
-            return ET.fromstring(response.content)
-        else:
-            raise ValueError("Unexpected content type: {}".format(response.headers.get('Content-Type')))
+        return response
+    
+    def _request_parse_xml(self, method: str, path: str, params: dict | None = None, data: bytes | None = None):
+        response = self._request(method, path, params, data)
+        if not response.headers.get('Content-Type') == 'application/xml':
+            raise RuntimeError("Did not receive XML response")
+        
+        return ET.fromstring(response.content)
 
     def list_buckets(self) -> list[Bucket]:
-        response = self._request('GET', '/')
+        response = self._request_parse_xml('GET', '/')
         buckets = []
         for bucket in response.findall('.//s3:Bucket', self.S3NS):
             name = bucket.find('s3:Name', self.S3NS).text
@@ -42,11 +42,138 @@ class S3:
             buckets.append(Bucket(name=name, creation_date=creation_date))
         return buckets
 
+    def list_objects(self, bucket_name: str, continuation_token: str | None = None) -> list[str]:
+        params = {
+            'list-type': 2,
+        }
+
+        if continuation_token:
+            params['continuation-token'] = continuation_token
+
+        path = f'/{bucket_name}'
+        response = self._request_parse_xml('GET', path, params)
+
+        objects = []
+        for obj in response.findall('.//s3:Contents', self.S3NS):
+            key = obj.find('s3:Key', self.S3NS).text
+            size = int(obj.find('s3:Size', self.S3NS).text)
+            last_modified = obj.find('s3:LastModified', self.S3NS).text
+            etag = obj.find('s3:ETag', self.S3NS).text.strip('"')
+            storage_class = obj.find('s3:StorageClass', self.S3NS).text
+            objects.append(Object(key=key, size=size, last_modified=last_modified, etag=etag, storage_class=storage_class))
+
+        # recursively fetch remaining objects
+        is_truncated = response.find('.//s3:IsTruncated', self.S3NS).text.lower() == 'true'
+        if is_truncated:
+            continuation_token = response.find('.//s3:NextContinuationToken', self.S3NS).text
+            objects.extend(self.list_objects(bucket_name, continuation_token))
+        
+        return objects
+
+    def put_object(self, bucket_name: str, key: str, data: bytes) -> None:
+        path = f'/{bucket_name}/{key}'
+        self._request('PUT', path, data=data)
+
+    def get_object_url(self, bucket_name: str, key: str) -> str:
+        path = f'/{bucket_name}/{key}'
+        return urljoin(self._endpoint_url, path)
+    
+    def delete_object(self, bucket_name: str, key: str) -> None:
+        path = f'/{bucket_name}/{key}'
+        self._request('DELETE', path)
+
+    def create_multipart_upload(self, bucket_name: str, key: str) -> MultipartUploadStream:
+        response = self._request_parse_xml('POST', f'/{bucket_name}/{key}?uploads')
+        upload_id = response.find('s3:UploadId', S3.S3NS).text
+
+        return MultipartUploadStream(
+            self,
+            bucket_name,
+            key,
+            upload_id
+        )
+
+class MultipartUploadStream():
+    def __init__(self, s3_client: S3, bucket: str, key: str, id: str):
+        self._client = s3_client
+        self.bucket = bucket
+        self.key = key
+        self.id = id
+        self._part_number = 0
+        self._completed = False
+
+        self._parts: list[str] = []
+
+    def upload_part(self, data: bytes) -> tuple[int, str]:
+        if self._completed:
+            raise RuntimeError("Cannot upload part to a completed multipart upload!")
+        
+        part_number = self._part_number
+        self._part_number += 1
+        params = {
+            'partNumber': part_number,
+            'uploadId': self.id
+        }
+
+        response = self._client._request('PUT', f'/{self.bucket}/{self.key}', params=params, data=data)
+        etag = response.headers.get("ETag")
+        if not etag:
+            raise RuntimeError("ETag not returned from UploadPart!")
+        
+        self._parts.insert(part_number, etag)
+        return (part_number, etag)
+    
+    def complete(self):
+        self._completed = True
+
+        # build CompleteMultipartUpload payload
+        root = ET.Element('CompleteMultipartUpload')
+        for part, etag in enumerate(self._parts):
+            part_elem = ET.SubElement(root, 'Part')
+            part_number_elem = ET.SubElement(part_elem, 'PartNumber')
+            part_number_elem.text = part
+            
+            etag_elem = ET.SubElement(part_elem, 'ETag')
+            etag_elem.text = etag
+        
+        xml = ET.tostring(root, encoding='unicode', xml_declaration=True)
+
+        params = {
+            'uploadId': self.id
+        }
+
+        response = self._client._request_parse_xml('POST', f'/{self.bucket}/{self.key}', params=params, data=xml.encode())
+        location = response.find('s3:Location', S3.S3NS).text
+        etag = response.find('s3:ETag', S3.S3NS).text
+
+        return CompleteMultipartUploadResult(
+            location,
+            self.bucket,
+            self.key,
+            etag
+        )
+
+
+    def abort(self):
+        self._completed = True
+        ...
+
 @dataclass
-class MultipartUpload:
-    ...
+class CompleteMultipartUploadResult:
+    location: str
+    bucket: str
+    key: str
+    etag: str
 
 @dataclass
 class Bucket:
     name: str
     creation_date: str
+
+@dataclass
+class Object:
+    key: str
+    size: int
+    last_modified: str
+    etag: str
+    storage_class: str
